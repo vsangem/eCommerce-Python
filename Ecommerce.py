4a3adab772e93b40bfdf993b40ec3d9002991755
vsangem <venkatjayasurya97@gmail.com>
2024-08-05 22:41:33 +0530

    Update Ecommerce.py

--- Ecommerce.py
+++ Ecommerce.py
@@ -804,6 +804,90 @@ driver.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
 driver.close()
 
 
+# Test Case 16: Place Order: Login before Checkout
+
+# 1. Launch browser
+driver = webdriver.Edge()
+driver.maximize_window()
+
+# 2. Navigate to url 'http://automationexercise.com'
+driver.get('https://www.automationexercise.com/')
+
+# 3. Verify that home page is visible successfully
+homepage = driver.find_element(By.XPATH, '//h1').text
+assert 'Automation' in homepage and 'Exercise' in homepage, f'Text not found in header, got: {homepage}'
+
+# 4. Click 'Signup / Login' button
+driver.find_element(By.LINK_TEXT, 'Signup / Login').click()
+
+# 5. Fill email, password and click 'Login' button
+name = 'Tulika'
+driver.find_element(By.XPATH, '//input[@data-qa="login-email"]').send_keys('Tulika.singh_20240803222313@yahoo.com')
+driver.find_element(By.XPATH, '//input[@data-qa="login-password"]').send_keys('QA@123')
+driver.find_element(By.XPATH, '//button[@data-qa="login-button"]').click()
+time.sleep(2)
+
+# 6. Verify 'Logged in as username' at top
+assert driver.find_element(By.CSS_SELECTOR, 'li a b').text == name
+
+# 7. Add products to cart
+actions = ActionChains(driver)
+driver.execute_script("window.scrollBy(0, 500);")
+first_product = driver.find_elements(By.CSS_SELECTOR, '.col-sm-4 div.product-image-wrapper')[2]
+actions.move_to_element(first_product).perform()
+driver.find_elements(By.CSS_SELECTOR, '.product-overlay div a')[2].click()
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, '.modal-content div:nth-child(3) button').click()
+second_product = driver.find_elements(By.CSS_SELECTOR, '.col-sm-4 div.product-image-wrapper')[3]
+actions.move_to_element(second_product).perform()
+driver.find_elements(By.CSS_SELECTOR, '.product-overlay div a')[3].click()
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, '.modal-content div:nth-child(3) button').click()
+
+# 8. Click 'Cart' button
+driver.find_element(By.LINK_TEXT, 'Cart').click()
+
+# 9. Verify that cart page is displayed
+assert driver.find_element(By.CSS_SELECTOR, '.breadcrumb li:nth-child(2)').text == 'Shopping Cart'
+
+# 10. Click Proceed To Checkout
+driver.find_element(By.CSS_SELECTOR, '.col-sm-6 a').click()
+
+# 11. Verify Address Details and Review Your Order
+assert driver.find_element(By.CSS_SELECTOR, 'li.address_firstname.address_lastname').text.__contains__('Tulika')
+review_your_order = driver.find_element(By.CSS_SELECTOR, '.cart_description h4 a')
+print('You\'re ordering: '+review_your_order.text)
+
+# 12. Enter description in comment text area and click 'Place Order'
+driver.find_element(By.CSS_SELECTOR, '.form-control').send_keys('Nothing to Add, Please Proceed!')
+driver.find_element(By.CSS_SELECTOR, '.btn.btn-default.check_out').click()
+
+# 13. Enter payment details: Name on Card, Card Number, CVC, Expiration date
+fake = Faker()
+driver.find_element(By.CSS_SELECTOR, 'input[data-qa="name-on-card"]').send_keys(fake.name_male())
+driver.find_element(By.CSS_SELECTOR, 'input[data-qa="card-number"]').send_keys(fake.credit_card_number())
+driver.find_element(By.CSS_SELECTOR, 'input[data-qa="cvc"]').send_keys(fake.credit_card_security_code())
+driver.find_element(By.CSS_SELECTOR, 'input[data-qa="expiry-month"]').send_keys('10')
+driver.find_element(By.CSS_SELECTOR, 'input[data-qa="expiry-year"]').send_keys('1999')
+
+# 14. Click 'Pay and Confirm Order' button
+driver.execute_script("window.scrollBy(0,500);")
+driver.find_element(By.CSS_SELECTOR, 'button[data-qa="pay-button"]').click()
+
+# 15. Verify success message 'Your order has been placed successfully!'
+# success_message_element = driver.find_element(By.CSS_SELECTOR, "div#success_message div")
+# success_message = success_message_element.text
+# expected_message = "Your payment has been successfully processed!"
+# assert success_message == expected_message, f"Expected message: '{expected_message}', but got: '{success_message}'"
+
+# 16. Click 'Delete Account' button
+driver.find_element(By.LINK_TEXT, 'Delete Account').click()
+
+# 17. Verify 'ACCOUNT DELETED!' and click 'Continue' button
+assert driver.find_element(By.CSS_SELECTOR, '.col-sm-9.col-sm-offset-1 h2').text == 'Account Deleted!'.upper()
+driver.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
+driver.close()
+
 
 
 
