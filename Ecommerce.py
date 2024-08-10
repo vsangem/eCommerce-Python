0c70d5cb2f9290eaba2d85adb814e826a07184d5
vsangem <venkatjayasurya97@gmail.com>
2024-08-10 23:26:30 +0530

    10082024 Add Test Case 20

--- Ecommerce.py
+++ Ecommerce.py
@@ -971,7 +971,63 @@ name = 'Tulika Singh'
 # assert "Men - "+random_options+" Products"
 
 
-# Test Case 19: View & Cart Brand Products
+# # Test Case 19: View & Cart Brand Products
+#
+# # 1. Launch browser
+# driver = webdriver.Edge()
+# driver.maximize_window()
+#
+# # 2. Navigate to url 'http://automationexercise.com'
+# driver.get('https://www.automationexercise.com/')
+#
+# # 3. Click on 'Products' button
+# driver.find_element(By.CSS_SELECTOR, 'a i.material-icons').click()
+#
+# # 4. Verify that Brands are visible on left sidebar
+# driver.execute_script("window.scrollBy(0, 500);")
+# assert driver.find_element(By.CSS_SELECTOR, '.brands_products h2').text == 'Brands'.upper()
+#
+# # 5. Click on any brand name
+# brand_names = driver.find_elements(By.CSS_SELECTOR, '.brands-name ul li a')
+# select_brand_name = [driver.execute_script("return arguments[0].textContent;", brand).strip() for brand in brand_names]
+# random_brand_selection = random.choice(select_brand_name)
+# print('1st Brand: ', format(random_brand_selection))
+# for brand in brand_names:
+#     driver.execute_script("arguments[0].scrollIntoView(true);", brand)
+#     text = driver.execute_script("return arguments[0].textContent;", brand).strip()
+#     if text == random_brand_selection:
+#         brand.click()
+#         break
+#
+# # 6. Verify that user is navigated to brand page and brand products are displayed
+# product_title = driver.find_element(By.CSS_SELECTOR, '.title.text-center').text
+# assert "Brand - "+random_brand_selection+" Products"
+# match = re.search(r'\((\d+)\)', random_brand_selection)
+# if match:
+#     displayed_count = int(match.group(1))
+#     print(f'Displayed Product Count: {displayed_count}')
+# else:
+#     print('Product count not found in the brand name text.')
+#     displayed_count = None
+#
+# # 7. On left sidebar, click on any other brand link
+# brand_names_2 = driver.find_elements(By.CSS_SELECTOR, '.brands-name ul li a')
+# select_brand_name_2 = [driver.execute_script("return arguments[0].textContent;", brand).strip() for brand in brand_names_2]
+# random_brand_selection_2 = random.choice(select_brand_name_2)
+# print('2nd Brand: ', format(random_brand_selection_2))
+# for brand_2 in brand_names_2:
+#     driver.execute_script("arguments[0].scrollIntoView(true);", brand_2)
+#     text = driver.execute_script("return arguments[0].textContent;", brand_2).strip()
+#     if text != random_brand_selection:
+#         brand_2.click()
+#         break
+#
+# # 8. Verify that user is navigated to that brand page and can see products
+# product_title_2 = driver.find_element(By.CSS_SELECTOR, '.title.text-center').text
+# assert "Brand - "+random_brand_selection_2+" Products"
+# driver.close()
+
+# Test Case 20: Search Products and Verify Cart After Login
 
 # 1. Launch browser
 driver = webdriver.Edge()
@@ -983,51 +1039,49 @@ driver.get('https://www.automationexercise.com/')
 # 3. Click on 'Products' button
 driver.find_element(By.CSS_SELECTOR, 'a i.material-icons').click()
 
-# 4. Verify that Brands are visible on left sidebar
-driver.execute_script("window.scrollBy(0, 500);")
-assert driver.find_element(By.CSS_SELECTOR, '.brands_products h2').text == 'Brands'.upper()
+# 4. Verify user is navigated to ALL PRODUCTS page successfully
+assert driver.find_element(By.CSS_SELECTOR, 'h2.title.text-center').text == 'All Products'.upper()
 
-# 5. Click on any brand name
-brand_names = driver.find_elements(By.CSS_SELECTOR, '.brands-name ul li a')
-select_brand_name = [driver.execute_script("return arguments[0].textContent;", brand).strip() for brand in brand_names]
-random_brand_selection = random.choice(select_brand_name)
-print('1st Brand: ', format(random_brand_selection))
-for brand in brand_names:
-    driver.execute_script("arguments[0].scrollIntoView(true);", brand)
-    text = driver.execute_script("return arguments[0].textContent;", brand).strip()
-    if text == random_brand_selection:
-        brand.click()
-        break
+# 5. Enter product name in search input and click search button
+all_products = driver.find_elements(By.CSS_SELECTOR, '.single-products div.productinfo p')
+product_text = [driver.execute_script("return arguments[0].textContent;", product).strip() for product in all_products]
+random_product_text = random.choice(product_text)
+driver.find_element(By.CSS_SELECTOR, 'input#search_product').send_keys(random_product_text)
+driver.find_element(By.CSS_SELECTOR, 'button#submit_search').click()
 
-# 6. Verify that user is navigated to brand page and brand products are displayed
-product_title = driver.find_element(By.CSS_SELECTOR, '.title.text-center').text
-assert "Brand - "+random_brand_selection+" Products"
-match = re.search(r'\((\d+)\)', random_brand_selection)
-if match:
-    displayed_count = int(match.group(1))
-    print(f'Displayed Product Count: {displayed_count}')
-else:
-    print('Product count not found in the brand name text.')
-    displayed_count = None
+# 6. Verify 'SEARCHED PRODUCTS' is visible
+assert driver.find_element(By.CSS_SELECTOR, 'h2.title.text-center').text == 'Searched Products'.upper()
 
-# 7. On left sidebar, click on any other brand link
-brand_names_2 = driver.find_elements(By.CSS_SELECTOR, '.brands-name ul li a')
-select_brand_name_2 = [driver.execute_script("return arguments[0].textContent;", brand).strip() for brand in brand_names_2]
-random_brand_selection_2 = random.choice(select_brand_name_2)
-print('2nd Brand: ', format(random_brand_selection_2))
-for brand_2 in brand_names_2:
-    driver.execute_script("arguments[0].scrollIntoView(true);", brand_2)
-    text = driver.execute_script("return arguments[0].textContent;", brand_2).strip()
-    if text != random_brand_selection:
-        brand_2.click()
-        break
+# 7. Verify all the products related to search are visible
+time.sleep(1.5)
+searchedProduct = driver.find_element(By.CSS_SELECTOR, '.productinfo.text-center p')
+searchedProduct.is_displayed()
+assert searchedProduct.text == random_product_text, f'random Product:{random_product_text},{searchedProduct}'
 
-# 8. Verify that user is navigated to that brand page and can see products
-product_title_2 = driver.find_element(By.CSS_SELECTOR, '.title.text-center').text
-assert "Brand - "+random_brand_selection_2+" Products"
+# 8. Add those products to cart
+actions = ActionChains(driver)
+first_product = driver.find_elements(By.CSS_SELECTOR, '.col-sm-4 div.product-image-wrapper')[0]
+actions.move_to_element(first_product).perform()
+driver.find_elements(By.CSS_SELECTOR, '.product-overlay div a')[0].click()
+time.sleep(1.5)
+driver.find_element(By.CSS_SELECTOR, '.modal-content div:nth-child(3) button').click()
 
+# 9. Click 'Cart' button and verify that products are visible in cart
+driver.find_element(By.LINK_TEXT, 'Cart').click()
+added_products = driver.find_element(By.CSS_SELECTOR, '.table.table-condensed tbody tr td:nth-child(2) h4').text
+assert random_product_text in added_products
 
+# 10. Click 'Signup / Login' button and submit login details
+driver.find_element(By.LINK_TEXT, 'Signup / Login').click()
+driver.find_element(By.XPATH, '//input[@data-qa="login-email"]').send_keys('Tulika.singh_20240803222313@yahoo.com')
+driver.find_element(By.XPATH, '//input[@data-qa="login-password"]').send_keys('QA@123')
+driver.find_element(By.XPATH, '//button[@data-qa="login-button"]').click()
 
+# 11. Again, go to Cart page
+driver.find_element(By.LINK_TEXT, 'Cart').click()
+
+# 12. Verify that those products are visible in cart after login as well
+assert driver.find_element(By.CSS_SELECTOR, '.table.table-condensed tbody tr td:nth-child(2) h4').text == random_product_text
 
 
 
