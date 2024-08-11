1a8c437e6ea009a0dd992a4cf7c68b9fbecaa1f4
vsangem <venkatjayasurya97@gmail.com>
2024-08-11 13:01:27 +0530

    10082024 Add Test Case 22

--- Ecommerce.py
+++ Ecommerce.py
@@ -1084,7 +1084,42 @@ fake = Faker()
 # assert driver.find_element(By.CSS_SELECTOR, '.table.table-condensed tbody tr td:nth-child(2) h4').text == random_product_text
 # driver.close()
 
-# Test Case 21: Add review on product
+# # Test Case 21: Add review on product
+#
+# # 1. Launch browser
+# driver = webdriver.Edge()
+# driver.maximize_window()
+#
+# # 2. Navigate to url 'http://automationexercise.com'
+# driver.get('https://www.automationexercise.com/')
+#
+# # 3. Click on 'Products' button
+# driver.find_element(By.CSS_SELECTOR, 'a i.material-icons').click()
+#
+# # 4. Verify user is navigated to ALL PRODUCTS page successfully
+# assert driver.find_element(By.CSS_SELECTOR, 'h2.title.text-center').text == 'All Products'.upper()
+#
+# # 5. Click on 'View Product' button
+# driver.execute_script("window.scrollTo(0, 500);")
+# driver.find_element(By.CSS_SELECTOR, '.col-sm-4 div.product-image-wrapper div[class="choose"] ul li a').click()
+#
+# # 6. Verify 'Write Your Review' is visible
+# assert driver.find_element(By.CSS_SELECTOR, '.col-sm-12 ul li a').text == 'Write Your Review'.upper()
+#
+# # 7. Enter name, email and review
+# driver.find_element(By.CSS_SELECTOR, '#name').send_keys(fake.name_male())
+# driver.find_element(By.CSS_SELECTOR,'#email').send_keys(fake.email())
+# driver.find_element(By.CSS_SELECTOR, '#review').send_keys(fake.text(100))
+#
+# # 8. Click 'Submit' button
+# driver.execute_script("window.scrollTo(0, 500);")
+# driver.find_element(By.CSS_SELECTOR, '#button-review').click()
+#
+# # 9. Verify success message 'Thank you for your review.'
+# assert driver.find_element(By.CSS_SELECTOR, '.alert-success.alert span').text == 'Thank you for your review.'
+
+
+# Test Case 22: Add to cart from Recommended items
 
 # 1. Launch browser
 driver = webdriver.Edge()
@@ -1093,33 +1128,30 @@ driver.maximize_window()
 # 2. Navigate to url 'http://automationexercise.com'
 driver.get('https://www.automationexercise.com/')
 
-# 3. Click on 'Products' button
-driver.find_element(By.CSS_SELECTOR, 'a i.material-icons').click()
+# 3. Scroll to bottom of page
+recommended_items = driver.find_elements(By.CSS_SELECTOR, '.title.text-center')
+if len(recommended_items) > 1:
+    recommended_item = recommended_items[1]
+    driver.execute_script("arguments[0].scrollIntoView(true);", recommended_item)
+    recommended_item_text = recommended_item.text
 
-# 4. Verify user is navigated to ALL PRODUCTS page successfully
-assert driver.find_element(By.CSS_SELECTOR, 'h2.title.text-center').text == 'All Products'.upper()
+# 4. Verify 'RECOMMENDED ITEMS' are visible
+    assert recommended_item_text == 'recommended items'.upper()
 
-# 5. Click on 'View Product' button
-driver.execute_script("window.scrollTo(0, 500);")
-driver.find_element(By.CSS_SELECTOR, '.col-sm-4 div.product-image-wrapper div[class="choose"] ul li a').click()
+# 5. Click on 'Add To Cart' on Recommended product
+recommended_products = driver.find_elements(By.XPATH, '//*[@id="recommended-item-carousel"]/div/div/div/div/div/div/p')
+if recommended_products:
+    random_product_index = random.randint(0, len(recommended_products) - 1)
+    selected_product = recommended_products[random_product_index]
+    selected_product_name = selected_product.text
+    add_to_cart_button = selected_product.find_element(By.XPATH, './following-sibling::a/i')
+    add_to_cart_button.click()
 
-# 6. Verify 'Write Your Review' is visible
-assert driver.find_element(By.CSS_SELECTOR, '.col-sm-12 ul li a').text == 'Write Your Review'.upper()
+# 6. Click on 'View Cart' button
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.modal-content div:nth-child(2) a'))).click()
 
-# 7. Enter name, email and review
-driver.find_element(By.CSS_SELECTOR, '#name').send_keys(fake.name_male())
-driver.find_element(By.CSS_SELECTOR,'#email').send_keys(fake.email())
-driver.find_element(By.CSS_SELECTOR, '#review').send_keys(fake.text(100))
-
-# 8. Click 'Submit' button
-driver.execute_script("window.scrollTo(0, 500);")
-driver.find_element(By.CSS_SELECTOR, '#button-review').click()
-
-# 9. Verify success message 'Thank you for your review.'
-assert driver.find_element(By.CSS_SELECTOR, '.alert-success.alert span').text == 'Thank you for your review.'
-
-
-
+# 7. Verify that product is displayed in cart page
+assert driver.find_element(By.CSS_SELECTOR, '.table.table-condensed tbody tr td:nth-child(2) h4').text == selected_product_name
 
 
 
